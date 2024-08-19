8e13fe01371f3f2dcab03241f77009fec6bbe4f9
Citrakaila <157669416+Citrakaila@users.noreply.github.com>
2024-08-19 14:33:09 +0700

    Add files via upload

---
+++ ctraaudi/dbctraaudi1.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3308
+-- Generation Time: Aug 19, 2024 at 04:34 AM
+-- Server version: 5.7.33
+-- PHP Version: 8.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbctraaudi1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbarang`
+--
+
+CREATE TABLE `tbarang` (
+  `id_barang` int(11) NOT NULL,
+  `kode` varchar(15) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `asal` varchar(25) NOT NULL,
+  `jumlah` int(4) NOT NULL,
+  `satuan` varchar(15) NOT NULL,
+  `tanggal_diterima` date NOT NULL,
+  `tanggal_simpan` timestamp NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbarang`
+--
+
+INSERT INTO `tbarang` (`id_barang`, `kode`, `nama`, `asal`, `jumlah`, `satuan`, `tanggal_diterima`, `tanggal_simpan`) VALUES
+(1, 'INV-2022-001', 'Meja Kantor', 'Pembelian', 1, 'Unit', '2022-06-01', '2024-08-19 03:59:04'),
+(2, 'INV-2022-001', 'Kursi Kantor', 'Hibah', 5, 'Unit', '2022-06-02', '2024-08-19 03:59:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbarang`
+--
+ALTER TABLE `tbarang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbarang`
+--
+ALTER TABLE `tbarang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
